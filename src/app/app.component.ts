0639fe6852a064eccfff16f302d2f0e92484f3f6
Darsh2126 <darsh2126gajdhar@gmail.com>
2023-10-26 22:18:58 +0530

    Learning Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'showtime';
+  title = "showtime";
+  navbg: any;
+  @HostListener("document:scroll") scrollover() {
+    console.log(document.body.scrollTop, "scrolllength#");
+
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.navbg = {
+        "background-color": "#000000",
+      };
+    } else {
+      this.navbg = {};
+    }
+  }
 }
